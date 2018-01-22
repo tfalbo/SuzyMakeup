8d071e42337cb049b90e9d39db38b4a9edce267a
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-01-22 00:57:18 -0200

    Trailing slash fix

--- vagrant/SuzyMakeup/finalProject.py
+++ vagrant/SuzyMakeup/finalProject.py
@@ -12,12 +12,12 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/categories')
+@app.route('/categories/')
 def showCategories():
     categories = session.query(Category)
     return render_template('categories.html', categories = categories)
 
-@app.route('/categories/new', methods=['GET', 'POST'])
+@app.route('/categories/new/', methods=['GET', 'POST'])
 def newCategory():
     if request.method == 'POST':
         newCategory = Category(name=request.form['name'])
@@ -27,7 +27,7 @@ def newCategory():
     else:
         return render_template('newCategory.html')
 
-@app.route('/categories/<int:category_id>/edit', methods=['GET', 'POST'])
+@app.route('/categories/<int:category_id>/edit/', methods=['GET', 'POST'])
 def editCategory(category_id):
     editedCategory = session.query(Category).filter_by(id=category_id).one()
     if request.method == 'POST':
@@ -39,7 +39,7 @@ def editCategory(category_id):
     else:
         return render_template('editCategory.html', category = editedCategory)
 
-@app.route('/categories/<int:category_id>/delete', methods=['GET', 'POST'])
+@app.route('/categories/<int:category_id>/delete/', methods=['GET', 'POST'])
 def deleteCategory(category_id):
     categoryToDelete = session.query(Category).filter_by(id=category_id).one()
     if request.method == 'POST':
@@ -53,14 +53,14 @@ def deleteCategory(category_id):
 ## Routes for Items
 
 @app.route('/categories/<int:category_id>')
-@app.route('/categories/<int:category_id>/items', methods = ['GET', 'POST'])
+@app.route('/categories/<int:category_id>/items/', methods = ['GET', 'POST'])
 def showItems(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Item).filter_by(category_id=category_id)
     return render_template('items.html', category=category, items=items, category_id=category_id)
 
 
-@app.route('/categories/<int:category_id>/items/new', methods = ['GET', 'POST'])
+@app.route('/categories/<int:category_id>/items/new/', methods = ['GET', 'POST'])
 def newItem(category_id):
     if request.method == 'POST':
         newItem = Item(name=request.form['name'],
@@ -73,7 +73,7 @@ def newItem(category_id):
     else:
         return render_template('newItem.html', category_id = category_id)
 
-@app.route('/categories/<int:category_id>/items/<int:item_id>/edit', methods = ['GET', 'POST'])
+@app.route('/categories/<int:category_id>/items/<int:item_id>/edit/', methods = ['GET', 'POST'])
 def editItem(category_id, item_id):
     editedItem = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
@@ -89,7 +89,7 @@ def editItem(category_id, item_id):
     else:
         return render_template('editItem.html', category_id = category_id, item = editedItem)
 
-@app.route('/categories/<int:category_id>/items/<int:item_id>/delete', methods = ['GET', 'POST'])
+@app.route('/categories/<int:category_id>/items/<int:item_id>/delete/', methods = ['GET', 'POST'])
 def deleteItem(category_id, item_id):
     itemToDelete = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
