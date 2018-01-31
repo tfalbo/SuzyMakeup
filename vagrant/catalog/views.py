e871fad6e568dfb6cf8c4a0608c39140ea8e7347
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-01-30 23:43:53 -0200

    set port

--- vagrant/catalog/views.py
+++ vagrant/catalog/views.py
@@ -179,4 +179,5 @@ def apiShowItems(category_id):
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port=port))
