bf2a09de1ec51fd494e2ecfe19cc12783210b3dc
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-01-30 23:44:40 -0200

    fix typo

--- vagrant/catalog/views.py
+++ vagrant/catalog/views.py
@@ -180,4 +180,4 @@ def apiShowItems(category_id):
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get('PORT', 5000))
-    app.run(host = '0.0.0.0', port=port))
+    app.run(host = '0.0.0.0', port=port)
