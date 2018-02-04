106604eaefa89134dae9ff8c7a83f6b7689f261f
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-02-03 22:07:03 -0200

    python3!!!

--- vagrant/catalog/views.py
+++ vagrant/catalog/views.py
@@ -92,7 +92,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
+        print ("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -127,7 +127,7 @@ def gconnect():
     output += login_session['picture']
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
-    print "done!"
+    print ("done!")
     return output
 
 
